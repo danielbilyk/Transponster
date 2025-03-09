f70a4940bf0e8fb5e0d415f82692f2662079d885
danielbilyk <bilykdaniel@icloud.com>
2025-03-09 14:31:21 +0100

    Added routing, docker-compose and 2GB file size limit for Apache

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 import logger_setup
-from flask import Flask, request
+from flask import Flask, request, redirect
 import logging
 from slack_events import app as slack_app, handler
 
 flask_app = Flask(__name__)
+
+@flask_app.route("/", methods=["GET"])
+def slack_events():
+	return redirect("https://github.com/danielbilyk/Transponster")
 
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
